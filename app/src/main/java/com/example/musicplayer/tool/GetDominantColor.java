4ff474d4e4d4fb70bc814c63cf58eb751a0e9b56
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2024-04-01 22:16:37 +0700

    Chức năng tìm kiếm, playbackstatus

--- app/src/main/java/com/example/musicplayer/tool/GetDominantColor.java
+++ app/src/main/java/com/example/musicplayer/tool/GetDominantColor.java
@@ -1,2 +1,31 @@
-package com.example.musicplayer.tool;public class GetDominantColor {
+package com.example.musicplayer.tool;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import androidx.palette.graphics.Palette;
+
+public class GetDominantColor {
+
+    public static int getDominantColor(byte[] imageBytes) {
+        if (imageBytes == null || imageBytes.length == 0) {
+            return 0x001111;
+        }
+
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+
+        Palette palette = Palette.from(bitmap).generate();
+
+
+        Palette.Swatch dominantSwatch = palette.getDominantSwatch();
+
+        if (dominantSwatch != null) {
+            return dominantSwatch.getRgb();
+        } else {
+            return 0x001111;
+        }
+    }
 }
+
