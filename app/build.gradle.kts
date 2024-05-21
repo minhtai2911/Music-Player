9c7e821de0f40a3631a588925b6564440f028345
DuyThong28 <duythong28112004@gmail.com>
2024-05-21 22:03:45 +0700

    save changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.musicplayer"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,6 @@ buildscript {
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.13" apply false
 }
